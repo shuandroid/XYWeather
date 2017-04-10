aca6c57090772a1967c6d7f77fee17fa89fbc868
zhoutao <zhoutao822@icloud.com>
2017-04-10 17:26:07 +0800

    setting fragment update

--- app/src/main/java/com/chen/xyweather/ui/SettingFragment.java
+++ app/src/main/java/com/chen/xyweather/ui/SettingFragment.java
@@ -1,15 +1,21 @@
 package com.chen.xyweather.ui;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Switch;
+import android.widget.TextView;
 
 import com.chen.xyweather.R;
 import com.chen.xyweather.base.BaseFragment;
 import com.chen.xyweather.view.drawer.BaseDrawer;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,16 +23,6 @@ import com.chen.xyweather.view.drawer.BaseDrawer;
  * create an instance of this fragment.
  */
 public class SettingFragment extends BaseFragment {
-
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
 
     public SettingFragment() {
@@ -48,38 +44,50 @@ public class SettingFragment extends BaseFragment {
         return null;
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SettingFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SettingFragment newInstance(String param1, String param2) {
-        SettingFragment fragment = new SettingFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+
+    @Bind(R.id.s_notice)
+    protected TextView tv_notice;
+
+    @Bind(R.id.s_changeskin)
+    protected TextView tv_changeskin;
+
+    @Bind(R.id.s_cleardata)
+    protected TextView tv_cleardata;
+
+    @Bind(R.id.s_about)
+    protected TextView tv_about;
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_setting, container, false);
+        View view = inflater.inflate(R.layout.fragment_setting, container, false);
+
+        ButterKnife.bind(this, view);
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()){
+                    case R.id.s_notice:
+                        break;
+                    case R.id.s_changeskin:
+                        break;
+                    case R.id.s_cleardata:
+                        break;
+                    case R.id.s_about:
+                        Intent intent=new Intent(getActivity(),MapActivity.class);
+                        startActivity(intent);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
+        return view;
     }
 
 }
