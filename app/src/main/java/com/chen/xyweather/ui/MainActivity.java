2c4a73dc5cf6d3d762e37c020877a10da2054d48
shuandroid <androidshu@qq.com>
2017-04-28 15:36:53 +0800

    add some listener.

--- app/src/main/java/com/chen/xyweather/ui/MainActivity.java
+++ app/src/main/java/com/chen/xyweather/ui/MainActivity.java
@@ -4,12 +4,17 @@ import android.content.Context;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.graphics.drawable.ColorDrawable;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Build;
+import android.view.MenuItem;
+import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
+import android.widget.Toast;
 
 import com.chen.xyweather.R;
 import com.chen.xyweather.base.BaseActivity;
@@ -17,6 +22,7 @@ import com.chen.xyweather.base.BaseFragment;
 import com.chen.xyweather.utils.DebugLog;
 import com.chen.xyweather.utils.DepthPageTransformer;
 import com.chen.xyweather.utils.UiUtil;
+import com.chen.xyweather.view.CircleImageView;
 import com.chen.xyweather.view.DynamicWeatherView;
 import com.chen.xyweather.view.DynamicWeatherViewTest;
 import com.chen.xyweather.view.drawer.BaseDrawer;
@@ -29,6 +35,7 @@ import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends BaseActivity {
 
@@ -49,6 +56,8 @@ public class MainActivity extends BaseActivity {
     @Bind(R.id.main_viewpager)
     protected MyViewPager mViewPager;
 
+    @Bind(R.id.navigation_view)
+    protected NavigationView mNavigation;
 
     @Override
     protected void setupContentView() {
@@ -74,9 +83,50 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void setupViews() {
 
+        setupNavigation();
         initAlpha();
 
         setupViewPager();
+    }
+
+    /**
+     * 设置侧滑栏
+     */
+    private void setupNavigation() {
+
+        View headerView = mNavigation.getHeaderView(0);
+        CircleImageView imageView = (CircleImageView) headerView.findViewById(R.id.nav_head_circle_view_header);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: 17-4-28 添加账号信息
+                Toast.makeText(getApplicationContext(), "header image view", Toast.LENGTH_SHORT).show();
+            }
+        });
+        //监听
+        mNavigation.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+
+                switch (item.getItemId()) {
+                    case R.id.nav_menu_setting:
+                        // TODO: 17-4-28 添加监听方法内容
+                        Toast.makeText(getApplicationContext(), "item setting", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.nav_add_city:
+                        break;
+                    case R.id.nav_feedback:
+                        break;
+                    case R.id.nav_about:
+                        break;
+                    case R.id.nav_menu_care:
+                        break;
+                    default:
+                        break;
+                }
+                return false;
+            }
+        });
     }
 
     public void setupViewPager() {
